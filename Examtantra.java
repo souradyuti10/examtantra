1c18eb171a9683043a077e9eb99ca6c94e437423
souradyuti10 <88334907+souradyuti10@users.noreply.github.com>
2021-12-03 10:50:58 +0530

    Add files via upload

---
+++ Examtantra.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examtantra;
+
+/**
+ *
+ * @author Subhra CHOUDHURY
+ */
+public class Examtantra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
